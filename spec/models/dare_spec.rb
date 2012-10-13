bbed0c7407e7f55bff3105e752247720f04298ef
p.bjorklund@gmail.com <p.bjorklund@gmail.com>
2012-10-14 00:16:36 +0200

    Refactor dare specs to take less space :bomb:

--- spec/models/dare_spec.rb
+++ spec/models/dare_spec.rb
@@ -20,27 +20,10 @@ describe Dare do
   end
 
   describe "#as_tweet" do
-
-    it 'returns a string' do
-      dare.as_tweet.should be_a(String)
-    end
-
-    it 'is specifically' do
-      dare.as_tweet.should eq('I #dare #anyone to run Stockholm marathon http://some.url.com/dares/12345')
-    end
-
-    it 'contains the daree nickname' do
-      dare.as_tweet.should match('#anyone')
-    end
-
-    it 'contains the dare text' do
-      dare.as_tweet.should match('run Stockholm marathon')
-    end
-
-    it 'contains a url' do
-      dare.as_tweet.should match(/http(s)?\:\/\/([a-z0-9\-\_]*)\.[a-z0-9\-\_]*\.[a-z0-9\-\_]{2,5}(\/(.*)?)?/i)
-    end
-
+    it { dare.as_tweet.should be_a(String) }
+    it { dare.as_tweet.should eq('I #dare #anyone to run Stockholm marathon http://some.url.com/dares/12345') }
+    it { dare.as_tweet.should match('#anyone') }
+    it { dare.as_tweet.should match('run Stockholm marathon') }
+    it { dare.as_tweet.should match(/http(s)?\:\/\/([a-z0-9\-\_]*)\.[a-z0-9\-\_]*\.[a-z0-9\-\_]{2,5}(\/(.*)?)?/i) }
   end
-
 end
