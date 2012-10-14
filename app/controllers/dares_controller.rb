43123a114a4b958388b2bd10dc93537de1f8d2df
eriksk <erikskoglund88@gmail.com>
2012-10-14 22:25:40 +0200

    fixed bug that caused owners to become darees

--- app/controllers/dares_controller.rb
+++ app/controllers/dares_controller.rb
@@ -22,7 +22,9 @@ class DaresController < ApplicationController
     @dare.state = params[:dare][:state]
 
     if @dare.state > Dare::STATES[:new] && current_user && (@dare.target == "@#{current_user.nickname}" or @dare.target.starts_with?("#") or @dare.owner == current_user)
-      @dare.dared_user = current_user
+      if (@dare.target == "@#{current_user.nickname}" or @dare.target.starts_with?("#")
+        @dare.dared_user = current_user
+      end
       if @dare.save
         flash[:success] = "State updated"
       else
