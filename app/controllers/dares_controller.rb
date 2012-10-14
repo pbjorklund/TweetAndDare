f45348a43dc9f7c1af502c9320e2db45684ade62
eriksk <erikskoglund88@gmail.com>
2012-10-15 01:20:41 +0200

    fixed™ bug with states AGAIN

--- app/controllers/dares_controller.rb
+++ app/controllers/dares_controller.rb
@@ -22,23 +22,37 @@ class DaresController < ApplicationController
     @dare = Dare.find(params[:id])
     @dare.state = params[:dare][:state]
 
-    if @dare.state == Dare::STATES[:accepted] && current_user == @dare.owner
-      flash[:error] = "You cannot accept this dare!"
-      redirect_to "/dares/#{params[:id]}"
-    elsif @dare.state > Dare::STATES[:new] && current_user && (@dare.target == "@#{current_user.nickname}" or @dare.target.starts_with?("#") or @dare.owner == current_user)
-      unless @dare.owner == current_user
-        @dare.dared_user = current_user
+    if current_user
+      if current_user == @dare.owner
+        # can set state to completed, failed IF state is accepted
+        if @dare.state == Dare::STATES[:completed] or @dare.state == Dare::STATES[:failed]
+          if @dare.save
+            flash[:success] = "State updated"
+          else
+            flash[:error] = "Error updating state"
+          end
+        else
+          flash[:error] = "You cannot accept this dare!"
+        end
+      elsif "@#{current_user.nickname}" == @dare.target or @dare.target.starts_with?("#")
+        # can set state to accepted IF state is new
+        if @dare.state == Dare::STATES[:accepted]
+          @dare.dared_user = current_user
+          if @dare.save
+            flash[:success] = "State updated"
+          else
+            flash[:error] = "Error updating state"
+          end
+        else
+          flash[:error] = "You cannot change this dare!"
+        end
       end
-      if @dare.save
-        flash[:success] = "State updated"
-      else
-        flash[:error] = "Could not update state"
-      end
-      redirect_to :action => :show, :id => params[:id]
     else
       session[:redirect_url] = "/dares/#{params[:id]}"
       redirect_to "/auth/twitter"
+      return
     end
+    redirect_to :action => :show, :id => params[:id]
   end
 
   private
